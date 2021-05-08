2a61e586391da2fc2e74574eb540c81d26455cc8
njm0623 <57427690+njm0623@users.noreply.github.com>
2021-05-08 22:21:49 +0900

    Update SaleBoardReplyDAOImpl.java

--- BestDesign/src/main/java/siat/bestdesign/saleboard/dao/SaleBoardReplyDAOImpl.java
+++ BestDesign/src/main/java/siat/bestdesign/saleboard/dao/SaleBoardReplyDAOImpl.java
@@ -10,41 +10,48 @@ import siat.bestdesign.saleboard.domain.SaleBoardDealVO;
 import siat.bestdesign.saleboard.domain.SaleBoardReplyVO;
 
 @Repository("saleBoardReplyDAO")
+// 드로잉샵 리뷰, 거래, 평점 
 public class SaleBoardReplyDAOImpl implements SaleBoardReplyDAO {
 	@Autowired
 	private SqlSessionTemplate mybatis;
 
 	@Override
+	// 드로잉샵 리뷰 등록 DAO
 	public Integer insertReply(SaleBoardReplyVO vo) {
 		System.out.println("===> Mybatis insertReply() 호출");	
 		return mybatis.insert("SaleBoardReplyDAO.insertReply", vo);
 	}
 
 	@Override
+	// 드로잉샵 리뷰 목록 불러오기 DAO
 	public List<SaleBoardReplyVO> selectAllReply(String saleNum) {
 		System.out.println("===> Mybatis selectAllReply() 호출");	
 		return mybatis.selectList("SaleBoardReplyDAO.selectAllReply", saleNum);
 	}
 
 	@Override
+	// 드로잉샵 리뷰 삭제 DAO
 	public int deleteReply(SaleBoardReplyVO vo) {
 		System.out.println("===> Mybatis deleteReply() 호출");	
 		return mybatis.delete("SaleBoardReplyDAO.deleteReply", vo);
 	}
 	
 	@Override
+	// 드로잉샵 리뷰 수정 DAO
 	public int modifyReply(SaleBoardReplyVO vo) {
 		System.out.println("===> Mybatis modifyReply() 호출");	
 		return mybatis.update("SaleBoardReplyDAO.modifyReply", vo);
 	}
 	
 	@Override
+	// 드로잉샵 가격 내역 등록 DAO
 	public void insertSaleBoardDeal(SaleBoardDealVO vo) {
 		System.out.println("===> Mybatis insertSaleBoardDeal() 호출");
 		mybatis.insert("SaleBoardReplyDAO.insertSaleBoardDeal", vo);
 	}
 	
 	@Override
+	// 드로잉샵 게시글 평점 갱신 DAO
 	public void updateSaleBoardRate(SaleBoardReplyVO vo) {
 		System.out.println("===> Mybatis modifyReply() 호출");	
 		mybatis.update("SaleBoardReplyDAO.updateSaleBoardRate", vo);
