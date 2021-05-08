a9f64e0963a5d7345046aeef010cd8c775075792
njm0623 <57427690+njm0623@users.noreply.github.com>
2021-05-08 22:00:17 +0900

    Update SaleBoardDealVO.java

--- BestDesign/src/main/java/siat/bestdesign/saleboard/domain/SaleBoardDealVO.java
+++ BestDesign/src/main/java/siat/bestdesign/saleboard/domain/SaleBoardDealVO.java
@@ -1,14 +1,14 @@
 package siat.bestdesign.saleboard.domain;
 
 public class SaleBoardDealVO {
-	private int dealNum;
-	private int saleNum;
-	private String saleTitle;
-	private String dealBuyerId;
-	private String dealSellerId;
-	private int dealPrice;
-	private String dealDate;
-	private String dealImage;
+	private int dealNum;		// 드로잉샵 거래 번호
+	private int saleNum;		// 드로잉샵 게시글 번호
+	private String saleTitle;	// 드로잉샵 게시글 제목
+	private String dealBuyerId;	// 드로잉샵 구매자 아이디
+	private String dealSellerId;	// 드로잉샵 판매자 아이디
+	private int dealPrice;		// 드로잉샵 거래 가격
+	private String dealDate;	// 드로잉샵 거래 날짜
+	private String dealImage;	// 드로잉샵 거래 
 	
 	public String getSaleTitle() {
 		return saleTitle;
