2c46364655b25aa2267ee14e847b35784f914544
njm0623 <57427690+njm0623@users.noreply.github.com>
2021-05-09 10:52:47 +0900

    Update SaleBoardReplyController.java

--- BestDesign/src/main/java/siat/bestdesign/saleboard/controller/SaleBoardReplyController.java
+++ BestDesign/src/main/java/siat/bestdesign/saleboard/controller/SaleBoardReplyController.java
@@ -26,40 +26,51 @@ public class SaleBoardReplyController {
 	@RequestMapping(value="reply/new.do", produces="application/text; charset=utf8")
 	
 	@ResponseBody
+	// 드로잉샵 댓글 추가 및 평점 갱신 컨트롤러
 	public void saleBoardReply(SaleBoardReplyVO vo) {
 		log.info("saleboard동작");
 		System.out.println("saleboard동작");
-	
+		
+		// 댓글 추가
 		saleBoardReplyService.insertReply(vo);
+		// 평점 갱신
 		saleBoardReplyService.updateSaleBoardRate(vo);
 	}
 	
 	@RequestMapping("reply.do")
 	@ResponseBody
+	// 드로잉샵 댓글 목록 불러오기 컨트롤러
 	public List<SaleBoardReplyVO> saleBoardReplyList(String saleNum) {
 		log.info("saleboard" + saleNum + "list동작");
 		System.out.println("saleboardlist동작" + saleNum);
-	
+		
+		// 댓글 목록 불러오기
 		return saleBoardReplyService.selectAllReply(saleNum);
 	}
 	
 	@RequestMapping("reply/delete.do")
 	@ResponseBody
+	// 드로잉샵 댓글 삭제 및 평점 갱신 컨트롤러
 	public void saleBoardReplyDelete(SaleBoardReplyVO vo) {
 		log.info("saleboard Delect동작");
 		System.out.println("saleboardlist Delect동작");
 		
+		// 댓글 삭제
 		saleBoardReplyService.deleteReply(vo);
+		// 평점 갱신
 		saleBoardReplyService.updateSaleBoardRate(vo);
 	}
 	
 	@RequestMapping(value="reply/modify.do", produces="application/text; charset=utf8")
 	@ResponseBody
+	// 드로잉샵 댓글 수정 및 평점 갱신 컨트롤러
 	public void saleBoardReplyModify(SaleBoardReplyVO vo) {
 		log.info("saleboard Modify동작");
 		System.out.println("saleboardlist Modify동작");
 		
+		// 댓글 수정
 		saleBoardReplyService.modifyReply(vo);
+		// 평점 갱신
 		saleBoardReplyService.updateSaleBoardRate(vo);
 	}
 }
